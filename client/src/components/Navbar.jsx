969a2a2f5ad423c0261b6611070275e7d931adb4
sujayvv <sujayvadavadagi@gmail.com>
2022-12-25 16:05:03 +0530

    NavBar Using Material UI

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+  LightModeOutlined,
+  DarkModeOutlined,
+  Menu as MenuIcon,
+  Search,
+  SettingsOutlined,
+  ArrowDropDownOutlined,
+} from "@mui/icons-material";
+import FlexBetween from "components/FlexBetween";
+import { useDispatch } from "react-redux";
+import { setMode } from "state";
+import profileImage from "assets/profile.jpeg"
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
